3f1afb39a134bae7a2ac2f954f44776c637a8903
uncleeesia <weiyang1112@hotmailcom>
2025-04-19 13:43:21 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("https://sim-assignment-csit314-9e613de15308.herokuapp.com//")
+      .then(res => setMessage(res.data.message))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <h1>React + Flask</h1>
+      <p>{message}</p>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <nav>
+            <h1>Welcome to the App</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
