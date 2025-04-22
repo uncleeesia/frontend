cb89e677c21c5f97f2fef005a2c2f1f44377ff16
uncleeesia <weiyang1112@hotmailcom>
2025-04-22 20:50:14 +0800

    update data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     axios.get("https://sim-assignment-csit314-9e613de15308.herokuapp.com/")
-      .then(res => setMessage(res.data.message))
+      .then(message => setMessage(message))
       .catch(err => console.error(err));
   }, []);
 
