b6e8e82427fe149cff165662cda9a15d5c4460dc
uncleeesia <weiyang1112@hotmailcom>
2025-04-22 21:09:03 +0800

    update return date with res.data.message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     axios.get("https://sim-assignment-csit314-9e613de15308.herokuapp.com/")
-      .then(data => setMessage(data.message))
+      .then(res => setMessage(res.data.message))
       .catch(err => console.error(err));
   }, []);
 
