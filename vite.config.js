1646ffdbb58bd58ca3ff340666d3b5622e0f9191
uncleeesia <weiyang1112@hotmailcom>
2025-05-03 19:37:16 +0800

    change config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://sim-assignment-csit314-9e613de15308.herokuapp.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
