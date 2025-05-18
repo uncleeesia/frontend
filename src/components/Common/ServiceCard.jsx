d81c148ab2bb9b6588924264cd6f92bcec8be9b5
uncleeesia <weiyang1112@hotmailcom>
2025-05-18 11:04:40 +0800

    update

--- src/components/Common/ServiceCard.jsx
+++ src/components/Common/ServiceCard.jsx
@@ -12,7 +12,7 @@ const ServiceCard = ({
 }) => {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate(`/service-details?by_user_id=${id}`);
+    navigate(`/service-details?service_id=${id}`);
   };
 
   return (
--- src/components/ViewHome/ViewHome.jsx
+++ src/components/ViewHome/ViewHome.jsx
@@ -183,7 +183,7 @@ const ViewHome = () => {
                   title={services.service_name}
                   content={services.service_description}
                   onClick={() => {
-                    navigate(`/service-details?id=${services.service_id}`);
+                    navigate(`/service-details?service_id=${services.service_id}`);
                   }}
                 />
               ))}
@@ -205,7 +205,7 @@ const ViewHome = () => {
               <ServiceCard
                 additionalClass="w-90"
                 key={index}
-                id={services.by_user_id}
+                id={services.service_id}
                 title={services.username}
                 description={services.service_description}
                 image="https://placehold.co/600x400"
